2b772252f3d3744074042bf1357895de323c10a3
jenleebeaver <jen.lee.beaver@gmail.com>
2021-01-01 08:54:04 -0800

    Update index.js
    addEventListener using AJAX fetch request to api/v1/users in our rails backend on localhost rails server. Append users to the json.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("in index.js");
+//this is our call to the user#index in our rails backend server
+const endPoint = "http://localhost:3000/api/v1/users"
+
+//fetching users 
+document.addEventListener('DOMContentLoaded', () => {
+    //AJAX fetch request 
+    fetch(endPoint)
+    //using response JSON to append elements to the DOM 
+    //.then returns promises.  If the promise is fulfilled or rejected THEN the handler function will be called asynchronously (scheduled in the thread loop).
+    .then(response => response.json()
+    .then(users => {
+        console.log(users);
+    }))
+});
