b0f1ecb51850e19ca51d45e91b9c64c6c52cf6ee
jenleebeaver <jen.lee.beaver@gmail.com>
2021-01-01 19:06:16 -0800

    Refactor
    Access recording data

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 //this is our call to the user#index in our rails backend server
-const endPoint = "http://localhost:3000/api/v1/users"
+const endPoint = "http://localhost:3000/api/v1/recordings"
 
 //fetching users 
 document.addEventListener('DOMContentLoaded', () => {
-    getUsers();
+    getRecordings();
 });
 
-function getUsers() {
+function getRecordings() {
        //AJAX fetch request 
        fetch(endPoint)
        //using response JSON to append elements to the DOM 
        //.then returns promises.  If the promise is fulfilled or rejected THEN the handler function will be called asynchronously (scheduled in the thread loop).
        .then(response => response.json()
-       .then(users => {
+       .then(recordings => {
            //call users.data.forEach because data is nested in the jsonserializer  
-           users.data.forEach(user => {
-             const userMarkup = `
-             <div data-id=${user.id}>
-                <h1>${user.attributes.name}</h1>
-                <h2>${user.attributes.recordings.title}</h2>
-                <h3>${user.attributes.recordings.melody}</h3>
-                <button data-id=${user.id}>edit</button>
+           recordings.data.forEach(recording => {
+             const recordingMarkup = `
+             <div data-id=${recording.id}>
+                <h1>${recording.attributes.title}</h1> 
+                <h2>${recording.attributes.melody}</h2>
+                <h3>${recording.attributes.user.name}</h3>
+                <button data-id=${recording.id}>edit</button>
             </div>
             </br>`;
-
+            console.log(recording)
             //updating the inner html with id user-container to show data in userMarkup 
-            document.querySelector('#user-container').innerHTML += userMarkup
+            document.querySelector('#recording-container').innerHTML += recordingMarkup
            });
        }))
 }
