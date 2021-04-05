64adb6eb21b51ce67b7693f9df096ebdafe19520
jenleebeaver <jen.lee.beaver@gmail.com>
2021-04-05 13:43:51 -0700

    index.js refactor
    create RecordingCard and change functions order

--- src/recording.js
+++ src/recording.js
@@ -1,9 +1,26 @@
 class Recording {
 
-    constructor(recording, recordingAttributes){
-        this.id = recording.id;
-        this.title = recordingAttributes.title;
-        this.audio_url = recordingAttributes.audio_url;
-        this.user = recordingAttributes.user.name;
+    constructor(id, title, audio_url, user_name){
+        this.id = id;
+        this.title = title;
+        this.audio_url = audio_url;
+        this.user = user_name;
+        Recording.all.push(this);
+        // debugger
+    }  
+
+    renderRecordingCard() {
+        // debugger
+        return `
+            <div data-id=${this.id}>
+                <h1>${this.title}</h1> 
+                <h2>${this.user}</h2>
+                <a>${this.audio_url}</a>
+                <br/>
+                <button data-id=${this.id}>edit</button>
+            </div>
+            </br>`;
     }
 }
+
+Recording.all = [];
